59941f2649b927464c5984e9cdcb806c3d7234e5
Krishna Suresh <krishna4132@gmail.com>
2022-02-25 10:03:10 -0500

    Reading data from jpg

--- src/jpegcompress.c
+++ src/jpegcompress.c
@@ -1,5 +1,33 @@
-int main(int argc, char const *argv[])
-{
-    // TODO Compress JPEG 
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[]) {
+    FILE *file;
+    file = fopen("jpegstructure.jpg", "rt");
+    unsigned char *buffer;
+    unsigned long fileLen;
+    // Get file length
+    fseek(file, 0, SEEK_END);
+    fileLen = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Allocate memory
+    buffer = (char *)malloc(fileLen);
+    if (!buffer) {
+        fprintf(stderr, "Memory error!");
+        fclose(file);
+        return 1;
+    }
+
+    fread(buffer, fileLen, sizeof(unsigned char), file);
+    fclose(file);
+
+    int i = 0;
+
+    while (i < fileLen) {
+           printf("%02X ",((unsigned char)buffer[i]));
+           i++;
+           if( ! (i % 16) ) printf( "\n" );
+    }
+
     return 0;
 }
