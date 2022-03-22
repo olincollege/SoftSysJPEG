dbbd78c66f40cc0165ecea453e328b6482643a63
Merwan <myeditha7@gmail.com>
2022-03-21 23:01:33 -0400

    refactored main code

--- src/read_bmp_to_array.c
+++ src/read_bmp_to_array.c
@@ -1,59 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "JPEG.h"
 
-unsigned char *read_bmp_to_array(char *file_location, int *width, int *height,
-                        int *padding, int *bitcount, int *size) {
-    unsigned char *data = 0;
-    unsigned char info[54] = {0};
-    FILE *file = fopen(file_location, "rb");
-    if (!file) {
-        return 0;
-    }
-    fread(info, 1, 54, file);
-    (*width) = *(int *)(info + 18);
-    *height = *(int *)(info + 22);
-    (*bitcount) = *(int *)(info + 28);
-    (*size) = (((*width) * (*bitcount) + 31) / 32) * 4 * (*height);
-    (*padding) = (*width) % 4;
-    if ((*bitcount) != 24) {  // this code works for 24-bit bitmap only
-        if (file) fclose(file);
-        if (data) free(data);
-        return 0;
-    }
+int read_bmp(BMP * bmp, Image * image);
+int read_bmp(BMP * bmp, Image * image) {
+    image->width = get_width(bmp);
+    image->height = get_height(bmp);
 
-    data = malloc((*size));
-    fread(data, 1, (*size), file);
-    return data;
-}
+    image->blockHeight = (image->height + 7) / 8; // TODO figure out why theres a plus 7 here
+    image->blockWidth = (image->width + 7) / 8;
+    printf("size | w:%i h:%i\n", image->width, image->height);
+    printf("blocks | w:%i h:%i \n", image->blockHeight, image->blockWidth);
+    image->blocks = malloc(sizeof(Block) * (image->blockHeight * image->blockWidth));
+    unsigned char r, g, b;
+    int k =0;
+    for (size_t i = image->height-1; i < image->height; i--)
+    {
+        const int blockRow = k / 8;
+        const int pixelRow = k % 8;
+        for (size_t j = 0; j < image->width; j++)
+        {
+            get_pixel_rgb(bmp, j, i, &r, &g, &b);
 
-void read_channels (unsigned char * data, int height, int width, int padding, int*red, int*green, int* blue) {
-    for (int row = height - 1; row >= 0; row--) {
-        for (int col = 0; col < width; col++) {
-            int p = (row * width + col) * 3 + row * padding;
-            *((red+row*width) + col) = data[p + 0];
-            *((green+row*width) + col) = data[p + 1];
-            *((blue+row*width) + col) = data[p + 2];
-            // red[row][col] = data[p + 0];
-            // green[row][col] = data[p + 1];
-            // blue[row][col] = data[p + 2];
+
+            const int blockColumn = j / 8;
+            const int pixelColumn = j % 8;
+            const int blockIndex = blockRow * image->blockWidth + blockColumn;
+            const int pixelIndex = pixelRow * 8 + pixelColumn;
+
+            image->blocks[blockIndex].r[pixelIndex] = r; // BGR instead of rgb, idk why
+            image->blocks[blockIndex].g[pixelIndex] = g; 
+            image->blocks[blockIndex].b[pixelIndex] = b;
         }
+        k++;
+        
     }
+    return 0;
+    
 }
 
-void rgb_to_ycrcb(int height, int width, int* red, int* green, int* blue, int* y, int* cb, int* cr){
-    int val;
+void rgb_to_ycrcb(Image * image){
     int r,g,b;
-    // r -> y
-    // g -> cb
-    // b -> cr
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
-            r = *((red+i*j)+j);
-            g = *((green+i*j)+j);
-            b = *((blue+i*j)+j);
-            *((y+i*j)+j) = 16+ (((r<<6)+(r<<1)+(g<<7)+g+(b<<4)+(b<<3)+b)>>8);
-            *((cb+i*j)+j) = 128 + ((-((r<<5)+(r<<2)+(r<<1))-((g<<6)+(g<<3)+(g<<1))+(b<<7)-(b<<1))>>8);
-            *((cr+i*j)+j) = 128 + (((r<<7)-(r<<4)-((g<<6)+(g<<5)-(g<<1))-((b<<4)+(b<<1)))>>8); 
+    for (int i = 0; i < image->blockHeight*image->blockWidth; i++){
+        for (int j = 0; j < 64; j++){
+            r = image->blocks[i].r[j];
+            g = image->blocks[i].g[j];
+            b = image->blocks[i].b[j];
+            image->blocks[i].y[j] = round(16.0 + (65.738/256.0)*(float)r + (129.057/256.0)*(float)g + (25.064/256.0)*(float)b);
+            image->blocks[i].cb[j] = round(128.0 + (-37.945/256.0)*(float)r + (-74.494/256.0)*(float)g + (112.439/256.0)*(float)b);
+            image->blocks[i].cr[j] = round(128.0 + (112.439/256.0)*(float)r + (-94.154/256.0)*(float)g + (-18.285/256.0)*(float)b);
         }
     }
 }
@@ -72,27 +67,5 @@ void chroma_downsample(int* cb, int* cr, int height, int width){
             *((cr+row*width)+col) = cr_avg;
             *((cr+row+1*width)+col) = cr_avg;
         }
-    }
-}
-
-int main(int argc, char const *argv[]) {
-    int width, height, padding, bitcount, size;
-    unsigned char  *data = read_bmp_to_array("include/data/test.bmp", &width, &height,
-                                   &padding, &bitcount, &size);
-    int red[height][width];
-    int green[height][width];
-    int blue[height][width];
-    read_channels(data, height, width, padding, (int*)red, (int*)green, (int*)blue);
-    int y[height][width];
-    int cb[height][width];
-    int cr[height][width];
-    rgb_to_ycrcb(height, width, (int*)red, (int*)green, (int*)blue, (int*)y, (int*)cb, (int*)cr);
-    int i, j;
-    // Print out array for testing
-    for (i = 0; i < height; i++) {
-        for (j = 0; j < width; j++) {
-            printf("%i ", red[i][j]);
-        }
-        printf("\n");
     }
 }
